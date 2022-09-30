b47dedc2634a93611d4c84c6c064e34cbbc73eba
melektron <matteo@elektron.work>
2022-09-30 23:31:18 +0200

    open cv image read test

--- image_capture.py
+++ image_capture.py
@@ -8,10 +8,15 @@ Module to handle capturing screenshots from the external video device
 """
 
 import os
+import cv2
 
 DEVICE_ID = 0
 
+camera = cv2.VideoCapture(DEVICE_ID)
+
 
 def take_picture(x=1920, y=1080, format="yuyv422", file="/tmp/out1.png") -> str:
-    os.system(f"ffmpeg -f v4l2 -video_size {x}x{y} -input_format {format} -i /dev/video{DEVICE_ID} -frames 1 {file}")
+    return_value, image = camera.read()
+    cv2.imwrite(file, image)
+    #os.system(f"ffmpeg -f v4l2 -video_size {x}x{y} -input_format {format} -i /dev/video{DEVICE_ID} -frames 1 {file}")
     return file
