6c99cb10859523969d237153871bee524d3cef43
melektron <matteo@elektron.work>
2022-10-01 17:39:10 +0200

    changed to cv2 method

--- image_capture.py
+++ image_capture.py
@@ -9,19 +9,19 @@ Module to handle capturing screenshots from the external video device
 
 import os
 import shutil
-#import cv2
+import cv2
 
-#DEVICE_ID = 0
-#
-#camera = cv2.VideoCapture(DEVICE_ID)
-#camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-#camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+DEVICE_ID = 0
+
+camera = cv2.VideoCapture(DEVICE_ID)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1360)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
 #camera.set(cv2.CAP_PROP_FORMAT, -1)
 
 
 def take_picture(x=1920, y=1080, format="yuyv422", file="./captures/new.png") -> str:
-    #return_value, image = camera.read()
-    #cv2.imwrite(file, image)
-    shutil.copy("./new.png", file)
+    return_value, image = camera.read()
+    cv2.imwrite(file, image)
+    #shutil.copy("./new.png", file)
     #os.system(f"ffmpeg -f v4l2 -video_size {x}x{y} -input_format {format} -i /dev/video{DEVICE_ID} -frames 1 {file}")
     return file
